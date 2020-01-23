743ecfcfe793bdc43fac47bc4a97cdb410340a61
felipegontijo <epilef.gontijo@gmail.com>
2020-01-23 12:59:04 -0600

    Require and use CORS

--- website/server.js
+++ website/server.js
@@ -13,6 +13,7 @@ const app = express();
 
 /* Dependencies */
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -20,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
