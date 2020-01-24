4dd44dfd1ce950826a9014f0c9c3d8d87d7eaf94
felipegontijo <epilef.gontijo@gmail.com>
2020-01-23 18:20:38 -0600

    Add function to GET project data

--- website/app.js
+++ website/app.js
@@ -9,7 +9,22 @@ let apiKey = '835a7c7c37c90166525f435f5bc6c601'
 
 /* Function to GET Web API Data*/
 
+
 /* Function to POST data */
 
 
 /* Function to GET Project Data */
+const getData = async (url = '') => {
+    // make the request to the url
+    const request = await fetch(url);
+
+    try {
+        // transform requested data into JSON if success
+        const newData = await request.json();
+        // return the transformed data
+        return newData;
+    } catch (error) {
+        // couldn't retrieve the requested data
+        console.log('error', error);
+    }
+}
