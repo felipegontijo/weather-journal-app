994ad712ddd27d5003780ad8a52804b33be44c79
felipegontijo <epilef.gontijo@gmail.com>
2020-01-30 11:20:37 -0600

    feat: fetch post data

--- website/app.js
+++ website/app.js
@@ -20,7 +20,22 @@ const getApiData = async (url, element, key) => {
 }
 
 /* Function to POST data */
-
+const postData = async (url = '', data = {}) => {
+    const res = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match 'Content-Type' header
+    });
+    try {
+        const newData = await res.json();
+        return newData;
+    } catch (error) {
+        console.log('error', error);
+    }
+}
 
 /* Function to GET Project Data */
 const getData = async (url = '') => {
